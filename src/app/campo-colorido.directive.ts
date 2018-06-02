68341f4fa2d39b00d236b775cbafd6777baee792
cmartinsgyn <cmartinsgyn@gmail.com>
2018-06-02 10:37:14 -0300

    aula de botão colorir e descolorir

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,28 +1,30 @@
-import { Directive, ElementRef, Renderer2, HostListener, HostBinding, Input } from '@angular/core';
+import { Directive, HostListener, HostBinding, Input } from '@angular/core';
 import { element } from 'protractor';
 
 @Directive({
-  selector: '[appCampoColorido]'
+  selector: '[appCampoColorido]',
+  exportAs: 'campoColorido'
 })
 export class CampoColoridoDirective {
 
-  constructor(
-    private elementRef: ElementRef,
-    private renderer: Renderer2
-  ) {}
+//  constructor(
+//     private elementRef: ElementRef,
+//     private renderer: Renderer2
+//   ) {}
 
-    @Input('appCampoColorido') cor = 'gray';
+   @Input('appCampoColorido') cor = 'gray';
 
    @HostBinding('style.backgroundColor') corDeFundo: string;
 
-   @HostListener('focus') aoGanharFoco() {
+   @HostListener('focus') colorir() {
     this.corDeFundo = this.cor;
    }
 
-   @HostListener('blur') aoPerderFoco() {
+   @HostListener('blur') descolorir() {
+    this.corDeFundo = 'transparent';
     // this.renderer.setStyle(this.elementRef.nativeElement,
     //   'background-color', 'yellow');
-    this.corDeFundo = 'transparent';
+
    }
 
 }
