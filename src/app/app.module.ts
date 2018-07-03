36e6dfa5114f19e4ab4437f232dd8bbf0f19ae1c
claudio <cmartinsgyn@gmail.com>
2018-07-03 00:52:44 -0300

    aula 14.2 - reexportando o modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
+import { BotoesModule } from './botoes/botoes.module';
+import { NavegacaoModule } from './navegacao/navegacao.module';
+import { AppComponent } from './app.component';
+import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
+import { FuncionarioFormComponent } from './funcionario-form/funcionario-form.component';
+import { CampoColoridoDirective } from './campo-colorido.directive';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { BotoesModule } from './botoes/botoes.module';
-import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
-import { FuncionarioFormComponent } from './funcionario-form/funcionario-form.component';
-import { CampoColoridoDirective } from './campo-colorido.directive';
 
 @NgModule({
   declarations: [
@@ -19,7 +19,7 @@ import { CampoColoridoDirective } from './campo-colorido.directive';
   imports: [
     BrowserModule,
     FormsModule,
-    BotoesModule
+    NavegacaoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
